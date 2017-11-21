a4ddbdbba2676d509b55d5dcf82acda43d1b4257
Naveen <metanaveen@outlook.com>
2017-11-22 01:33:40 +0530

    Databinding - Property Binding

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
+  }
+
+  ngOnInit() {
+    // setTimeout(() => {
+    //   this.allowNewServer = true
+    // }, 2000);
+  }
 }

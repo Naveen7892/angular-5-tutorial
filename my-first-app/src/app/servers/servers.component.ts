a420041ca1f5c8f312653fe0887ce5353c1bb1fc
Naveen <metanaveen@outlook.com>
2017-11-22 00:01:12 +0530

    Component templates,styles and selectors as element,class & attribute

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,8 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  // template: `<app-server></app-server>
+  
+  // <hr>
+  // Reused components!
+  // <hr>
+  
+  // <app-server></app-server>`,
+  // styleUrls: ['./servers.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
 
